ce5a7cc470eb283e0eb9a0accfcd45cdee20144f
Peter Gombert <66751150+petegombert@users.noreply.github.com>
2024-02-12 13:35:13 -0700

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,7 @@
+"""
+This is the downloader for the repository. There are only the raw data scripts in here now but in the future I'll add other scripts
+If it is necessary to get other scripts, just add the raw path to the list of scripts. 
+"""
+import requests
+
+oned_corr_path = ''
