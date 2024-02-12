d8de27406514ed9c61ef8f157d30f6b1463ee3cd
Peter Gombert <66751150+petegombert@users.noreply.github.com>
2024-02-12 12:02:19 -0700

    Create call_main.py

---
+++ call_main.py
@@ -0,0 +1,12 @@
+"""
+This script will be the main form of interaction with repositiory. Run this code to connect with other scripts
+"""
+import oned_corr
+import zonal
+
+#*****This is where the dataset is selected. id=1 is the zonal dataset & id=2 is the global cloud occurrence dataset.
+dataset_id=1
+dbz_sel = 
+
+if __name__ == '__main__':
+  
